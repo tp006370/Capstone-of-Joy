5c7cbe76f50678d432dafb4e771a838d078f9d81
Tim Paul <tp006370@comcast.net>
2017-09-15 21:08:14 -0400

    Update for Week 4

--- BotTestTests/ProgramTests.cs
+++ BotTestTests/ProgramTests.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Connector.DirectLine;
 using System.Threading;
+using System.Data.SqlClient;
 
 namespace BotTest.Tests
 {
@@ -103,7 +104,15 @@ namespace BotTest.Tests
         {
             String input = "What is my GPA";
             String response = GetBotResponseTest(input);
-            Assert.AreEqual(response, "Your GPA is: 4.33");
+            StringAssert.Contains(response, "Your GPA is: 3.34");
+        }
+
+        [TestMethod()]
+        public void repeatRequestCummulativeGPATest()
+        {
+            String input = "What is my GPA";
+            String response = GetBotResponseTest(input);
+            StringAssert.Contains(response, "Your GPA is: 3.34");
         }
 
 
@@ -114,7 +123,6 @@ namespace BotTest.Tests
             String response = GetBotResponseTest(input);
             StringAssert.Contains(response, "3.2");
         }
-
 
         [TestMethod()]
         public void requestAnothersGPA()
@@ -137,9 +145,63 @@ namespace BotTest.Tests
         {
             String input = "Hello Bot, how when does SWENG500 end";
             String response = GetBotResponseTest(input);
-            StringAssert.Contains(response, "Never");
+            StringAssert.Contains(response, "12/3/2017");
         }
 
+        [TestMethod()]
+        public void requestCourseTeacher()
+        {
+            String input = "Hello Bot, who teaches SWENG500";
+            String response = GetBotResponseTest(input);
+            StringAssert.Contains(response, "Robert Stephens");
+        }
+
+        [TestMethod()]
+        public void requestProfeseorRating()
+        {
+            String input = "What is Professor Stephens rating";
+            String response = GetBotResponseTest(input);
+            StringAssert.Contains(response, "9");
+        }
+
+        [TestMethod()]
+        public void requestClassBasedOnSubject()
+        {
+            String input = "Bit is there a class on Software System Design";
+            String response = GetBotResponseTest(input);
+            StringAssert.Contains(response, "SWENG537");
+        }
+
+        [TestMethod()]
+        public void requestProfessorEmailAddress()
+        {
+            String input = "Bot what is Professor's Stephens email address";
+            String response = GetBotResponseTest(input);
+            StringAssert.Contains(response, "rstevens@psu.edu");
+        }
+
+ /*       [TestMethod()]
+        public void TestGPACaluation4_0()
+        {
+
+            ****** IN WORK *********
+            SqlConnection connection = new SqlConnection("Data Source = sweng500.database.windows.net,1433; Initial Catalog = SWENG500; Integrated Security = False; User ID = sweng500; Password = sweng500; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+  
+            SqlCommand UpdateGPA = new SqlCommand();
+            UpdateGPA = connection.CreateCommand();
+            UpdateGPA.CommandText = @"UPDATE dbo.AcademicHistory SET Grade=A" + " WHERE AcademicHistoryId=2";
+
+            connection.Open();
+            UpdateGPA.ExecuteNonQuery();
+            connection.Close();
+
+
+            String input = "Bot what is my GPA";
+            String response = GetBotResponseTest(input);
+            StringAssert.Contains(response, "4.0");
+        }
+
+    */
         public string GetBotResponseTest(String input)
         {
             //Create an activity to recieved the returned text
